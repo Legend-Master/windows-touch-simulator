b3e7d547ddb19e553e189c457e59d2fcbf3c84da
Tony <legendmastertony@gmail.com>
2024-06-27 17:49:54 +0800

    Init

---
+++ note.md
@@ -0,0 +1,1 @@
+https://github.com/microsoft/Windows-classic-samples/blob/main/Samples/InjectTouchPointerInput/cpp/InjectTouch.cpp
---
+++ src/main.rs
@@ -0,0 +1,144 @@
+use std::{sync::Mutex, thread::sleep, time::Duration};
+use windows::{
+    core::w,
+    Win32::{
+        Foundation::{HWND, LPARAM, LRESULT, POINT, WPARAM},
+        System::LibraryLoader::GetModuleHandleW,
+        UI::{
+            HiDpi::{SetProcessDpiAwarenessContext, DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2},
+            Input::{
+                KeyboardAndMouse::{GetAsyncKeyState, VIRTUAL_KEY, VK_SHIFT},
+                Pointer::{
+                    InitializeTouchInjection, InjectTouchInput, POINTER_FLAGS, POINTER_FLAG_DOWN,
+                    POINTER_FLAG_INCONTACT, POINTER_FLAG_INRANGE, POINTER_FLAG_UP,
+                    POINTER_FLAG_UPDATE, POINTER_INFO, POINTER_TOUCH_INFO, TOUCH_FEEDBACK_DEFAULT,
+                },
+            },
+            WindowsAndMessaging::{
+                CallNextHookEx, GetMessageW, SetWindowsHookExW, HHOOK, LLMHF_INJECTED, MSG,
+                MSLLHOOKSTRUCT, PT_TOUCH, WH_MOUSE_LL, WM_LBUTTONDOWN, WM_LBUTTONUP, WM_MOUSEMOVE,
+            },
+        },
+    },
+};
+
+macro_rules! log_error {
+    ($expression:expr) => {
+        if let Err(error) = $expression {
+            println!("{error}");
+        }
+    };
+}
+
+static mut CURRENT_TOUCH_INFOS: Mutex<Vec<POINTER_TOUCH_INFO>> = Mutex::new(Vec::new());
+
+fn main() {
+    unsafe { SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2).unwrap() };
+    let hmod = unsafe { GetModuleHandleW(w!("kernel32.dll")).unwrap() };
+    unsafe { SetWindowsHookExW(WH_MOUSE_LL, Some(low_level_mouse_proc), hmod, 0).unwrap() };
+
+    unsafe { InitializeTouchInjection(2, TOUCH_FEEDBACK_DEFAULT).unwrap() }
+
+    // keep touch contacts alive
+    std::thread::spawn(|| loop {
+        sleep(Duration::from_millis(100));
+        let touch_infos = unsafe { CURRENT_TOUCH_INFOS.lock().unwrap() };
+        if !touch_infos.is_empty() {
+            unsafe { log_error!(InjectTouchInput(&touch_infos)) };
+        }
+    });
+
+    let message: *mut MSG = std::ptr::null_mut();
+    while unsafe { GetMessageW(message, HWND::default(), 0, 0).into() } {}
+}
+
+unsafe extern "system" fn low_level_mouse_proc(
+    code: i32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    if code < 0 {
+        return CallNextHookEx(HHOOK::default(), code, wparam, lparam);
+    }
+
+    let info = *(lparam.0 as *const MSLLHOOKSTRUCT);
+    // dbg!(&wparam);
+    // dbg!(&info);
+    if info.flags & LLMHF_INJECTED != 0 {
+        println!("injected, info.flags: {}", info.flags);
+        return CallNextHookEx(HHOOK::default(), code, wparam, lparam);
+    }
+    match wparam.0 as u32 {
+        WM_LBUTTONDOWN => {
+            println!("sending touch down");
+            let touch_info = make_touch_info(
+                &info.pt,
+                POINTER_FLAG_DOWN | POINTER_FLAG_INRANGE | POINTER_FLAG_INCONTACT,
+            );
+            let mut touch_infos = vec![touch_info];
+            if is_key_down(VK_SHIFT) {
+                let mut second_contact = touch_info.clone();
+                second_contact.pointerInfo.pointerId = 1;
+                touch_infos.push(second_contact);
+            };
+            let result = InjectTouchInput(&touch_infos);
+            if result.is_ok() {
+                for touch_info in touch_infos.iter_mut() {
+                    touch_info.pointerInfo.pointerFlags =
+                        POINTER_FLAG_UPDATE | POINTER_FLAG_INRANGE | POINTER_FLAG_INCONTACT;
+                }
+                *CURRENT_TOUCH_INFOS.lock().unwrap() = touch_infos;
+            } else {
+                println!("{result:?}");
+            }
+            return LRESULT(1);
+        }
+        WM_MOUSEMOVE => {
+            let mut touch_infos = unsafe { CURRENT_TOUCH_INFOS.lock().unwrap() };
+            if let Some(touch_info) = touch_infos.last_mut() {
+                touch_info.pointerInfo.ptPixelLocation = info.pt;
+                log_error!(InjectTouchInput(&touch_infos));
+            }
+        }
+        WM_LBUTTONUP => {
+            let mut touch_infos = unsafe { CURRENT_TOUCH_INFOS.lock().unwrap() };
+            if !touch_infos.is_empty() {
+                println!("sending touch up");
+                for touch_info in touch_infos.iter_mut() {
+                    touch_info.pointerInfo.pointerFlags = POINTER_FLAG_UP;
+                }
+                log_error!(InjectTouchInput(&touch_infos));
+                touch_infos.clear();
+                return LRESULT(1);
+            }
+        }
+        _ => {}
+    };
+
+    CallNextHookEx(HHOOK::default(), code, wparam, lparam)
+}
+
+fn make_touch_info(point: &POINT, flags: POINTER_FLAGS) -> POINTER_TOUCH_INFO {
+    POINTER_TOUCH_INFO {
+        pointerInfo: POINTER_INFO {
+            pointerType: PT_TOUCH,
+            ptPixelLocation: *point,
+            pointerFlags: flags,
+            // pointerId: 1,
+            // dwTime: info.time,
+            ..Default::default()
+        },
+        // rcContact: RECT {
+        //     left: point.x,
+        //     right: point.x,
+        //     top: point.y,
+        //     bottom: point.y,
+        // },
+        // touchMask: TOUCH_MASK_CONTACTAREA,
+        ..Default::default()
+    }
+}
+
+fn is_key_down(key: VIRTUAL_KEY) -> bool {
+    unsafe { GetAsyncKeyState(key.0.into()) & 1 != 0 }
+}
